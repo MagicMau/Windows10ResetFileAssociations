916bb829e8780ca827a3b2601dc1c847397ed963
Maurits Elbers <MElbers@deloitte.nl>
2017-01-31 18:00:00 +0100

    A small utility using UI Automation to reset the Windows default programs (because my GPO settings keep changing them)

--- ResetFileAssociations/Program.cs
+++ ResetFileAssociations/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Automation;
 using System.Windows.Forms;
 
 namespace ResetFileAssociations
@@ -14,9 +17,89 @@ namespace ResetFileAssociations
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            SetSystemDefaultBrowserWithGUI("Microsoft Edge");
+        }
+
+        public static bool SetSystemDefaultBrowserWithGUI(string aBrowserName)
+        {
+            Process.Start("ms-settings:about");
+
+            var window = WaitForWindow("Settings");
+            if (window == null)
+                return false; // did not find in 10 seconds
+
+            // find all the invokable stuff here
+            var cond = new AndCondition(
+                new PropertyCondition(AutomationElement.NameProperty, "Default apps"),
+                new PropertyCondition(AutomationElement.IsSelectionItemPatternAvailableProperty, true));
+
+            var defaultAppsButton = window.FindFirst(TreeScope.Descendants, cond);
+            ((SelectionItemPattern)defaultAppsButton?.GetCurrentPattern(SelectionItemPattern.Pattern))?.Select();
+
+            if (!WaitForLabel(window, "Choose default apps"))
+                return false; // did not go as expected
+
+            // find the reset button
+            var resetBtnCond = new PropertyCondition(AutomationElement.NameProperty, "Reset");
+            var resetBtn = window.FindFirst(TreeScope.Descendants, resetBtnCond)?.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+
+            if (resetBtn == null)
+                return false;
+
+            resetBtn.Invoke();
+
+            // close settings
+            ((WindowPattern)window.GetCurrentPattern(WindowPattern.Pattern)).Close();
+
+            return true;
+        }
+
+
+        public static AutomationElement WaitForWindow(string title)
+        {
+            AutomationElement mainWindow = null;
+            for (int attempts = 0; mainWindow == null && attempts < 100; Thread.Sleep(100), attempts++)
+            {
+                mainWindow = GetSpecificWindow(title);
+            }
+
+            return mainWindow;
+        }
+
+        public static bool WaitForLabel(AutomationElement window, string title)
+        {
+            AutomationElement label = null;
+            var condition = new PropertyCondition(AutomationElement.NameProperty, "Choose default apps");
+            for (int attempts = 0; label == null && attempts < 100; Thread.Sleep(100), attempts++)
+            {
+                label = window.FindFirst(TreeScope.Descendants, condition);
+            }
+
+            return label != null;
+        }
+
+
+        public static AutomationElement GetSpecificWindow(string aWinTitle)
+        {
+            AutomationElement mainWindow = null;
+            AutomationElementCollection winCollection = AutomationElement.RootElement.FindAll(TreeScope.Children, Condition.TrueCondition);
+
+            foreach (AutomationElement ele in winCollection)
+            {
+                if (ele.Current.Name.ToLower() == aWinTitle.ToLower())
+                {
+                    mainWindow = ele;
+                    break;
+                }
+            }
+            return mainWindow;
+        }
+
+        public static AutomationElement GetSpecificAutomationItem(string aWinTitle, string itemName)
+        {
+            AutomationElement window = GetSpecificWindow(aWinTitle);
+            Condition condition = new PropertyCondition(AutomationElement.NameProperty, itemName);
+            return window.FindFirst(TreeScope.Element | TreeScope.Descendants, condition);
         }
     }
 }
